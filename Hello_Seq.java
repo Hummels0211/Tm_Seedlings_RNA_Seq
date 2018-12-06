deb0f8908af466818e06b9deae75d6fc5ea3b65e
Hummels0211 <yeveshummels@gmail.com>
2018-12-06 12:40:52 +0000

    The test and description of the project

---
+++ Hello_Seq.java
@@ -0,0 +1,8 @@
+public class Hello_Seq {
+
+	public static void main(String[] args) {
+		// Prints "Hello, Seq!" and the description of this pack
+		System.out.println("Hello, Seq! This is the pack for RNA_Seq analysis.");
+	}
+
+}
