42e6c6d4a67d32531fdcbf62881e9f3166369a81
Jia Yu <yeveshummels@gmail.com>
2018-12-06 13:24:56 +0000

    Update RNA_seq_analysis.py

--- RNA_seq_analysis.py
+++ RNA_seq_analysis.py
@@ -1,8 +1,21 @@
-import scipy as sp
-import numpy as np
+# import necessary packages
 import csv
+import os
 
-df = pd.read_csv('\RNA_Seq_Seedlings\RNAseq20180228Arabidopsis.csv')
+# Change the file directory to read and write the data
+os.chdir("C://Users/giaccoyu/Documents/GitHub/Tm_Seedlings_RNA_Seq/RNA_Seq_Seedlings")
+
+# Load the original csv data of RNA copies
+df = open('RNAseq20180228Arabidopsis.csv')
+data = csv.reader(df)
+
+# Create a new file to store all the relative RNA expression of the transcriptome
+with open('rna_expression.csv', 'w+') as rnaprofile:
+    writer = csv.write(rnaprofile)
+    writer.writecolumn([''])
+
+# Create a new columns for storing the calculations in the same csv file
+df.write()
 
 
 #####################
